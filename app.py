677bb78d06965dfaae3849acc5b6f4ccee207424
Bicky Eric Kantona <bickyeric@ymail.com>
2020-01-18 15:29:25 +0700

    fix deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hai Semua!!!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='7654')
