2532960d2f6a954825723175b4792e3afd2a2e93
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-02-16 13:52:05 +0700

    replace content

--- app/api/main.go
+++ app/api/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "OK Berhasil")
+		return c.String(http.StatusOK, "SELAMAT DATANG")
 	})
 	e.Logger.Fatal(e.Start(":2001"))
 }
